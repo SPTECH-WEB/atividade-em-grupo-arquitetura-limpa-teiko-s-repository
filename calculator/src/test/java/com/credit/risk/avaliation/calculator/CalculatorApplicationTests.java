6e48a5e33c1431d53e7aca2327717690e199553c
PajorVi <vinicius.marques@sptech.school>
2025-08-26 19:39:29 -0300

    add config inicial

---
+++ calculator/src/test/java/com/credit/risk/avaliation/calculator/CalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.credit.risk.avaliation.calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
